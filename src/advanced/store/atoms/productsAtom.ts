8dac0ee93ff5dc357395abf498034ce45dff2416
minsu <>
2025-12-05 01:47:29 +0900

    add: 배포파일 업로드

--- src/advanced/store/atoms/productsAtom.ts
+++
@@ -1,8 +0,0 @@
-import { atomWithStorage } from "jotai/utils";
-import { ProductWithUI, STORAGE_KEYS, INITIAL_PRODUCTS } from "../../constants";
-
-// 기본 상태
-export const productsAtom = atomWithStorage<ProductWithUI[]>(
-  STORAGE_KEYS.PRODUCTS,
-  INITIAL_PRODUCTS
-);
