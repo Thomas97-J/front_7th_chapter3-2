101c4b4cef6fda5cdcc82fe52470eb3559697847
minsu <>
2025-12-05 00:27:41 +0900

    add: jotai 아톰, 액션 추가

---
+++ src/advanced/store/index.ts
@@ -0,0 +1,23 @@
+// Cart
+export {
+  cartAtom,
+  totalItemCountAtom,
+  isCartEmptyAtom,
+} from "./atoms/cartAtom";
+
+// Products
+export { productsAtom } from "./atoms/productsAtom";
+
+// Coupons
+export {
+  couponsAtom,
+  selectedCouponAtom,
+  couponCountAtom,
+} from "./atoms/couponsAtom";
+
+// Notifications
+export {
+  notificationsAtom,
+  hasNotificationsAtom,
+  type Notification,
+} from "./atoms/notificationAtom";
